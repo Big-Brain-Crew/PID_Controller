83386a228ee2acb62810500c558b84bd6cc44265
Swapnil Pande <swapnil.pande98@gmail.com>
2022-02-25 22:46:32 -0500

    Limited max integrator value and reset integrator on parameter change

--- C++/pid_controller.cpp
+++ C++/pid_controller.cpp
@@ -1,35 +1,35 @@
 //*********************************************************************************
 // Arduino PID Library Version 1.0.1 Modified Version for C++
 // Platform Independent
-// 
+//
 // Revision: 1.1
-// 
+//
 // Description: The PID Controller module originally meant for Arduino made
 // platform independent. Some small bugs present in the original Arduino source
 // have been rectified as well.
-// 
+//
 // For a detailed explanation of the theory behind this library, go to:
 // http://brettbeauregard.com/blog/2011/04/improving-the-beginners-pid-introduction/
-// 
+//
 // Revisions can be found here:
 // https://github.com/tcleg
-// 
+//
 // Modified by: Trent Cleghorn , <trentoncleghorn@gmail.com>
-// 
+//
 // Copyright (C) Brett Beauregard , <br3ttb@gmail.com>
-// 
+//
 //                                 GPLv3 License
-// 
-// This program is free software: you can redistribute it and/or modify it under 
-// the terms of the GNU General Public License as published by the Free Software 
-// Foundation, either version 3 of the License, or (at your option) any later 
+//
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
 // version.
-// 
-// This program is distributed in the hope that it will be useful, but WITHOUT ANY 
-// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
 // PARTICULAR PURPOSE.  See the GNU General Public License for more details.
-// 
-// You should have received a copy of the GNU General Public License along with 
+//
+// You should have received a copy of the GNU General Public License along with
 // this program.  If not, see <http://www.gnu.org/licenses/>.
 //*********************************************************************************
 
@@ -48,8 +48,8 @@
 //*********************************************************************************
 
 PIDControl::
-PIDControl (float kp, float ki, float kd, float sampleTimeSeconds, float minOutput, 
-            float maxOutput, PIDMode mode, PIDDirection controllerDirection)     	
+PIDControl (float kp, float ki, float kd, float sampleTimeSeconds, float minOutput,
+            float maxOutput, PIDMode mode, PIDDirection controllerDirection)
 {
     controllerDirection = controllerDirection;
     this->mode = mode;
@@ -58,7 +58,7 @@ PIDControl (float kp, float ki, float kd, float sampleTimeSeconds, float minOutp
     lastInput = 0.0f;
     output = 0.0f;
     setpoint = 0.0f;
-    
+
     if(sampleTimeSeconds > 0.0f)
     {
         sampleTime = sampleTimeSeconds;
@@ -68,13 +68,13 @@ PIDControl (float kp, float ki, float kd, float sampleTimeSeconds, float minOutp
         // If the passed parameter was incorrect, set to 1 second
         sampleTime = 1.0f;
     }
-    
+
     PIDOutputLimitsSet(minOutput, maxOutput);
     PIDTuningsSet(kp, ki, kd);
 }
-        
+
 bool PIDControl::
-PIDCompute() 
+PIDCompute()
 {
     float error, dInput;
 
@@ -82,33 +82,33 @@ PIDCompute()
     {
         return false;
     }
-    
+
     // The classic PID error term
     error = setpoint - input;
-    
+
     // Compute the integral term separately ahead of time
     iTerm += alteredKi * error;
-    
+
     // Constrain the integrator to make sure it does not exceed output bounds
     iTerm = CONSTRAIN(iTerm, outMin, outMax);
-    
+
     // Take the "derivative on measurement" instead of "derivative on error"
     dInput = input - lastInput;
-    
+
     // Run all the terms together to get the overall output
     output = alteredKp * error + iTerm - alteredKd * dInput;
-    
+
     // Bound the output
     output = CONSTRAIN(output, outMin, outMax);
-    
+
     // Make the current input the former input
     lastInput = input;
-    
+
     return true;
 }
-     
+
 void PIDControl::
-PIDModeSet(PIDMode mode)                                                                                                                                       
+PIDModeSet(PIDMode mode)
 {
     // If the mode changed from MANUAL to AUTOMATIC
     if(mode != mode && mode == AUTOMATIC)
@@ -116,54 +116,54 @@ PIDModeSet(PIDMode mode)
         // Initialize a few PID parameters to new values
         iTerm = output;
         lastInput = input;
-        
+
         // Constrain the integrator to make sure it does not exceed output bounds
         iTerm = CONSTRAIN(iTerm, outMin, outMax);
     }
-    
+
     mode = mode;
 }
 
 void PIDControl::
-PIDOutputLimitsSet(float min, float max) 							  							  
+PIDOutputLimitsSet(float min, float max)
 {
     // Check if the parameters are valid
     if(min >= max)
     {
         return;
     }
-    
+
     // Save the parameters
     outMin = min;
     outMax = max;
-    
+
     // If in automatic, apply the new constraints
     if(mode == AUTOMATIC)
     {
         output = CONSTRAIN(output, min, max);
-        iTerm  = CONSTRAIN(iTerm,  min, max);
+        iTerm  = CONSTRAIN(iTerm,  0.2*min, 0.2*max);
     }
 }
 
 void PIDControl::
-PIDTuningsSet(float kp, float ki, float kd)         	                                         
+PIDTuningsSet(float kp, float ki, float kd)
 {
     // Check if the parameters are valid
     if(kp < 0.0f || ki < 0.0f || kd < 0.0f)
     {
         return;
     }
-    
+
     // Save the parameters for displaying purposes
     dispKp = kp;
     dispKi = ki;
     dispKd = kd;
-    
+
     // Alter the parameters for PID
     alteredKp = kp;
     alteredKi = ki * sampleTime;
     alteredKd = kd / sampleTime;
-    
+
     // Apply reverse direction to the altered values if necessary
     if(controllerDirection == REVERSE)
     {
@@ -171,6 +171,9 @@ PIDTuningsSet(float kp, float ki, float kd)
         alteredKi = -(alteredKi);
         alteredKd = -(alteredKd);
     }
+
+    // Reset integrator so that it does not accumulate error
+    iTerm = 0.0f;
 }
 
 void PIDControl::
@@ -192,7 +195,7 @@ PIDTuningKdSet(float kd)
 }
 
 void PIDControl::
-PIDControllerDirectionSet(PIDDirection controllerDirection)	  									  									  									  
+PIDControllerDirectionSet(PIDDirection controllerDirection)
 {
     // If in automatic mode and the controller's sense of direction is reversed
     if(mode == AUTOMATIC && controllerDirection == REVERSE)
@@ -202,12 +205,12 @@ PIDControllerDirectionSet(PIDDirection controllerDirection)
         alteredKi = -(alteredKi);
         alteredKd = -(alteredKd);
     }
-    
+
     controllerDirection = controllerDirection;
 }
 
 void PIDControl::
-PIDSampleTimeSet(float sampleTimeSeconds)                                                       									  									  									   
+PIDSampleTimeSet(float sampleTimeSeconds)
 {
     float ratio;
 
@@ -217,7 +220,7 @@ PIDSampleTimeSet(float sampleTimeSeconds)
         ratio = sampleTimeSeconds / sampleTime;
         alteredKi *= ratio;
         alteredKd /= ratio;
-        
+
         // Save the new sampling time
         sampleTime = sampleTimeSeconds;
     }
